9199190ccb04ba3f33d14f37f763a4f52ce285d6
Juan Pablo Gaviria Orozco <141966367+Darkblack595@users.noreply.github.com>
2023-09-04 22:36:44 -0500

    saas

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -34,5 +34,5 @@ class Grupo:
         cls.grado = nombre
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
+    def asignarNombre(cls, nombre="remoto"):
         cls.grado = nombre
